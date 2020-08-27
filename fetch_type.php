c91a99e9a136614f98224fe37198edd88c210cb9
epernorbert <eper.norbert98@gmail.com>
2020-08-27 13:23:27 +0200

    hide Típus option from values

--- fetch_type.php
+++ fetch_type.php
@@ -7,7 +7,7 @@
     $output = '';
     $sql = "SELECT * FROM car_type WHERE brands_id = '".$_POST["brandsId"]."' ORDER BY type_name";
     $result = mysqli_query($conn, $sql);
-    $output = '<option value="">Típus</option>';
+    $output = '<option value="" hidden="">Típus</option>';
     while($row = mysqli_fetch_array($result)){
         $output .= '<option value="'.$row['type_name'].'">'.$row['type_name'].'</option>';
     }
